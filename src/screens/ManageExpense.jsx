82de5fdf9c39470b6a99a7c3823db31b2244fb58
RRFayad <renan.r.fayad@gmail.com>
2024-02-18 11:38:29 -0300

    Manage Expense - Delete Section

--- src/screens/ManageExpense.jsx
+++ src/screens/ManageExpense.jsx
@@ -1,5 +1,8 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+
+import IconButton from "../components/ExpensesOutput/UI/IconButton";
+import { GlobalStyles } from "../shared/constants";
 
 function ManageExpense({ route, navigation }) {
   const expenseId = route.params?.id; // This will no throw an error, it will return undifined if there's no params
@@ -11,7 +14,20 @@ function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  return <Text>This is the Manage Expense Screen!</Text>;
+  return (
+    <View className="flex-1 bg-primary-800 p-6">
+      {isEditing && (
+        <View className="mt-4 items-center border-t-2 border-t-primary-200 pt-2">
+          <IconButton
+            icon={"trash"}
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={() => {}}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
