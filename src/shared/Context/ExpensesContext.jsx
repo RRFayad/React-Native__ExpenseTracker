181e970592efdc2b00d6a6d020bd194d650c3846
RRFayad <renan.r.fayad@gmail.com>
2024-02-20 07:18:55 -0300

    Expenses Context Logic

--- src/shared/Context/ExpensesContext.jsx
+++ src/shared/Context/ExpensesContext.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useReducer } from "react";
 
 import { DUMMY_EXPENSES } from "../data/DummyExpenses";
 
@@ -9,17 +9,57 @@ const ExpensesContext = React.createContext({
   updateExpense: (id, { description, amount, date }) => {},
 });
 
+const expensesReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      const id = Date.now().toString();
+      return [{ ...action.payload, id }, ...state];
+    case "UPDATE":
+      const toBeUpdatedExpenseIndex = state.findIndex(
+        (expense) => expense.id === action.payload.id,
+      );
+      const toBeUpdatedExpense = state[toBeUpdatedExpenseIndex];
+      const updatedExpense = {
+        ...toBeUpdatedExpense,
+        ...action.payload.expenseData,
+      };
+      const newStateArr = [...state];
+      newStateArr[toBeUpdatedExpenseIndex] = updatedExpense;
+      return newStateArr;
+
+    case "DELETE":
+      return state.filter((expense) => expense.id !== action.payload);
+    default:
+      return state;
+  }
+};
+
 export function ExpensesContextProvider({ children }) {
-  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
-  const deleteExpense = () => {
+  // const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const [expensesState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES);
+
+  const addExpense = (expenseData) => {
+    dispatch({ type: "ADD", payload: expenseData });
+  };
+
+  const updateExpense = (id, expenseData) => {
+    dispatch({ type: "UPDATE", payload: expenseData });
+  };
+
+  const deleteExpense = (id) => {
+    dispatch({ type: "DELETE", payload: id });
     navigation.goBack();
   };
-  const addExpense = () => {};
-  const updateExpense = () => {};
 
   return (
     <ExpensesContext.Provider
-      value={{ expenses, addExpense, deleteExpense, updateExpense }}
+      value={{
+        expenses: expensesState,
+        addExpense,
+        deleteExpense,
+        updateExpense,
+      }}
     >
       {children}
     </ExpensesContext.Provider>
