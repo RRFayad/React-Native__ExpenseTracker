45e8f6bb88298b7f2e3d567c7b8560a1493d0cbf
RRFayad <renan.r.fayad@gmail.com>
2024-02-22 11:19:36 -0300

    Manage Expense Form Input Handler

--- src/components/ManageExpense/CustomInput.jsx
+++ src/components/ManageExpense/CustomInput.jsx
@@ -1,9 +1,11 @@
 import { View, Text, TextInput } from "react-native";
 
-function CustomInput({ label, textInputConfig }) {
+function CustomInput({ label, textInputConfig, containerClassName }) {
   return (
-    <View className="mx-1 my-2">
-      <Text className="mb-1 text-xs text-primary-100">{label}</Text>
+    <View className={`mx-1 my-2 ${containerClassName}`}>
+      <Text className={`mb-1 text-center text-xs text-primary-100 `}>
+        {label}
+      </Text>
       <TextInput
         className={`rounded-md bg-primary-100 p-2 text-lg text-primary-700 ${textInputConfig.multiline && "min-h-[100px] align-text-top"}`}
         style={textInputConfig.multiline && { textAlignVertical: "top" }}
--- src/components/ManageExpense/ExpenseForm.jsx
+++ src/components/ManageExpense/ExpenseForm.jsx
@@ -1,29 +1,56 @@
-import { View, TextInput } from "react-native";
+import { useState } from "react";
+import { View, TextInput, Text } from "react-native";
 import CustomInput from "./CustomInput";
 
 function ExpenseForm() {
-  const amountChangeHandler = () => {};
+  const [inputValues, setInputValues] = useState({
+    amount: "",
+    date: "",
+    description: "",
+  });
+
+  const inputChangeHandler = (inputIdentifier, enteredText) => {
+    setInputValues((prevState) => {
+      return { ...prevState, [inputIdentifier]: enteredText };
+    });
+    console.log(inputIdentifier, enteredText);
+  };
+
   return (
-    <View>
-      <CustomInput
-        label={"Amount"}
-        textInputConfig={{
-          keyboardType: "decimal-pad",
-          onChangeText: amountChangeHandler,
-        }}
-      />
-      <CustomInput
-        label={"Date"}
-        textInputConfig={{
-          placeholder: "YYYY-MM-DD",
-          maxLength: 10,
-          onChangeText: () => {},
-        }}
-      />
+    <View className="mt-4">
+      <Text className="my-4 text-center text-2xl font-bold text-white">
+        Your Expense
+      </Text>
+      <View className="flex-row justify-between">
+        <CustomInput
+          label={"Amount"}
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: (enteredText) =>
+              inputChangeHandler("amount", enteredText), // onChangeText gives the text as the arg for the handler fn (so we don't need to pass it)
+            value: inputValues.amount,
+          }}
+          containerClassName={"flex-1"}
+        />
+        <CustomInput
+          label={"Date"}
+          textInputConfig={{
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChangeText: (enteredText) =>
+              inputChangeHandler("date", enteredText), // onChangeText gives the text as the arg for the handler fn (so we don't need to pass it)
+            value: inputValues.date,
+          }}
+          containerClassName={"flex-1"}
+        />
+      </View>
       <CustomInput
         label={"Description"}
         textInputConfig={{
           multiline: true,
+          onChangeText: (enteredText) =>
+            inputChangeHandler("description", enteredText), // onChangeText gives the text as the arg for the handler fn (so we don't need to pass it)
+          value: inputValues.description,
         }}
       />
     </View>
